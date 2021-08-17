4d245bb104070d26c6efb406417c0ea4a1542cbb
pawelszpunar <78647588+pawelszpunar@users.noreply.github.com>
2021-08-17 16:14:09 +0200

    initial commit

---
+++ src/main/java/com/pawelszpunar/myblogapp/MyBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.pawelszpunar.myblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawelszpunar/myblogapp/MyBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawelszpunar.myblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
